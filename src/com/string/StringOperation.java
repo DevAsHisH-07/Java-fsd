3c7e51ebd9b25e26803171d9cf951ac39d3273d6
Devashish <devashish19jha@gmail.com>
2023-10-24 17:18:50 +0530

    All files Added

--- src/com/string/StringOperation.java
+++ src/com/string/StringOperation.java
@@ -6,25 +6,25 @@ public class StringOperation {
 		//string variable initialization
 		String string_var1="string function program";
 		String string_var2="string function";
-		
+
 		// Commonly used String function
 		System.out.println("String operation are:");
 		// Getting character at specific position
 		System.out.println(string_var1.charAt(5));
-		
+
 		//getting unicode character at specific function
 		System.out.println(string_var1.codePointAt(3));
 		//Getting the no. of uni code character b/w given index
 		System.out.println(string_var1.codePointCount(0,5));
 		//Getting the no. of uni code character before given index
 		System.out.println(string_var1.codePointBefore(2));
-		
+
 		//String Comparison
 		System.out.println("/n String Comparision");
 		System.out.println(string_var1.compareTo(string_var2));
 		//comparing by ignoring case
 		System.out.println(string_var1.compareToIgnoreCase(string_var2));
-		
+
 		//String Concatenation
 		System.out.println(string_var1.concat(string_var2));
 		//check if string contains other string
@@ -42,61 +42,61 @@ public class StringOperation {
 		System.out.println(string_var1.indexOf("program"));
 		// check if the string is empty
 		System.out.println(string_var1.isEmpty());
-		
+
 		//last occurrence of specific character
 		System.out.println("/nLast Occurrence/n");
 		System.out.println(string_var1.lastIndexOf("function"));
-		
+
 		//length of string
 		System.out.println(string_var1.length());
-		
+
 		//replace
 		System.out.println(string_var1.replace("function", "operation"));
-		
+
 		//verifies if string starts with 
 		System.out.println(string_var1.startsWith("string"));
-		
+
 		//upper and lower case
 		System.out.println(string_var1.toUpperCase());
 		String str1="UPPER";
 		System.out.println(str1.toLowerCase());
-		
+
 		//trim
 		String str2="  Hello World   ";
 		System.out.println(str2);
 		System.out.println(str2.trim());
-		
+
 		//StringBuilder
-				System.out.println("\n");
-				System.out.println("Creating StringBuilder");
-				StringBuilder sb1=new StringBuilder("Happy");
-				sb1.append("Learning");
-				System.out.println(sb1);
+		System.out.println("\n");
+		System.out.println("Creating StringBuilder");
+		StringBuilder sb1=new StringBuilder("Happy");
+		sb1.append("Learning");
+		System.out.println(sb1);
 
-				System.out.println(sb1.delete(0, 1));
+		System.out.println(sb1.delete(0, 1));
 
-				System.out.println(sb1.insert(1, "Welcome"));
+		System.out.println(sb1.insert(1, "Welcome"));
 
-				System.out.println(sb1.reverse());
-				
-				//conversion	
-				System.out.println("\n");
-				System.out.println("Conversion of Strings to StringBuffer and StringBuilder");
+		System.out.println(sb1.reverse());
 
-				String str = "Hello"; 
+		//conversion	
+		System.out.println("\n");
+		System.out.println("Conversion of Strings to StringBuffer and StringBuilder");
 
-				// conversion from String object to StringBuffer 
-				StringBuffer sbr = new StringBuffer(str); 
-				sbr.reverse(); 
-				System.out.println("String to StringBuffer");
-				System.out.println(sbr); 
+		String str = "Hello"; 
 
-				// conversion from String object to StringBuilder 
-				StringBuilder sbl = new StringBuilder(str); 
-				sbl.append("world"); 
-				System.out.println("String to StringBuilder");
-				System.out.println(sbl);           
-		
+		// conversion from String object to StringBuffer 
+		StringBuffer sbr = new StringBuffer(str); 
+		sbr.reverse(); 
+		System.out.println("String to StringBuffer");
+		System.out.println(sbr); 
+
+		// conversion from String object to StringBuilder 
+		StringBuilder sbl = new StringBuilder(str); 
+		sbl.append("world"); 
+		System.out.println("String to StringBuilder");
+		System.out.println(sbl);           
+
 	}
 
 }
