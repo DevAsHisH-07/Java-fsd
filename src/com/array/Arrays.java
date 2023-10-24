c1cc582327f3775a8f852526ffbb20d5f4928564
Devashish <devashish19jha@gmail.com>
2023-10-24 17:25:31 +0530

    All files Added

--- src/com/array/Arrays.java
+++ src/com/array/Arrays.java
@@ -4,7 +4,7 @@ public class Arrays {
 	public static void main(String[] args) {
 
 		//single-dimensional array
-		int arr[]= {100,200,300,400,500,600,700};
+		int arr[]= {120,210,350,456,572,622,790};
 		for(int i=0;i<5;i++) {
 			System.out.println("Elements of array a: "+arr[i]);
 		}
@@ -12,8 +12,8 @@ public class Arrays {
 
 		//multidimensional array
 		int[][] multiArr = {
-				{5, 3, 9, 10}, 
-				{10, 5, 7,1} };
+				{4, 2, 8, 9}, 
+				{11, 7, 6,2} };
 
 		System.out.println("\nLength of row 1: " + multiArr[0].length);
 	}
